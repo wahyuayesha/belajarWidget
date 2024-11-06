2955355fa5a15374b0d20bc13800354eb78db084
M Wahyu Ayesha <mwahyuayesha@gmail.com>
2024-11-06 11:41:15 +0700

    Login page

--- main.dart
+++ main.dart
@@ -14,22 +14,62 @@ class MyApp extends StatelessWidget {
       // Root widget
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('My Home Page'),
+          title: const Text('Login Page'),
         ),
         body: Center(
           child: Builder(
             builder: (context) {
-              return Column(
-                children: [
-                  const Text('Hello, World!'),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      print('Click!');
-                    },
-                    child: const Text('A button'),
-                  ),
-                ],
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        FlutterLogo(
+                          size: 80,
+                        ),
+                        Text(
+                          'Flutter',
+                          style: TextStyle(fontSize: 36),
+                        ),
+                      ],
+                    ),
+
+                    // Input Email
+                    Container(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: const TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Email',
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 21, horizontal: 12)),
+                      ),
+                    ),
+
+                    // Input Password
+                    Container(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: const TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Password',
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 21, horizontal: 12)),
+                      ),
+                    ),
+
+                    // Forgot password text button
+                    forgotButton(),
+                    // Login button
+                    loginButton(),
+                    Container(
+                        margin: EdgeInsets.all(20),
+                        child: Text('New User? Create Account'))
+                  ],
+                ),
               );
             },
           ),
@@ -37,6 +77,36 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+}
+
+Widget forgotButton() {
+  return Container(
+    padding: const EdgeInsets.only(bottom: 10),
+    child: TextButton(
+        style: ButtonStyle(
+            foregroundColor: WidgetStateProperty.all<Color>(Colors.blue)),
+        onPressed: () {},
+        child: const Text('Forgot Password')),
+  );
+}
+
+Widget loginButton() {
+  return SizedBox(
+    height: 45,
+    width: 240,
+    child: ElevatedButton(
+      style: const ButtonStyle(
+          backgroundColor: WidgetStatePropertyAll(Colors.blue),
+          foregroundColor: WidgetStatePropertyAll(Colors.white)),
+      onPressed: () {
+        print('Clicked!');
+      },
+      child: const Text(
+        'Login',
+        style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+      ),
+    ),
+  );
 }
 
 class MyHomePage extends StatefulWidget {
@@ -73,12 +143,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         // TRY THIS: Try changing the color here to a specific color (to
