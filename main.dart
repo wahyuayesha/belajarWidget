52f1c398b29a70985075aa12ab078459e70a7eb3
M Wahyu Ayesha <mwahyuayesha@gmail.com>
2024-11-11 20:04:24 +0700

    Container

--- main.dart
+++ main.dart
@@ -14,99 +14,82 @@ class MyApp extends StatelessWidget {
       // Root widget
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Login Page'),
+          title: const Text(''),
         ),
         body: Center(
-          child: Builder(
-            builder: (context) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Row(
+            child: Container(
+          height: 540,
+          width: double.infinity,
+          margin: const EdgeInsets.all(30),
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 238, 234, 229),
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.3),
+                  spreadRadius: 2,
+                  blurRadius: 8,
+                  offset: const Offset(0, 4),
+                ),
+              ]),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Title: The Eras Tour',
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 90, 76, 56)),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              const Divider(
+                color: Colors.black,
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              const Center(
+                  child: Image(
+                      image: NetworkImage(
+                          'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/158648e4-fb58-4b77-8053-e91ff4e194e4/dfs72qc-0f7c939b-6e66-423c-885d-48b85927909a.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzE1ODY0OGU0LWZiNTgtNGI3Ny04MDUzLWU5MWZmNGUxOTRlNFwvZGZzNzJxYy0wZjdjOTM5Yi02ZTY2LTQyM2MtODg1ZC00OGI4NTkyNzkwOWEuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.2WDe4vbpgDei81mSEsmts3m9kXYutICywXxOs36rp2k',
+                          scale: 5))),
+              const SizedBox(height: 20),
+              const Text(
+                "Description: The Eras Tour is Taylor Swift's 6th tour. It is predicted to become the highest grossing tour ever on December 8, 2024, with a total attendance of 10 million.",
+                style: TextStyle(color: Color.fromARGB(255, 94, 79, 58)),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                  height: 40,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        FlutterLogo(
-                          size: 80,
+                        Icon(
+                          Icons.menu_book,
+                          color: Color.fromARGB(255, 94, 79, 58),
                         ),
-                        Text(
-                          'Flutter',
-                          style: TextStyle(fontSize: 36),
+                        SizedBox(
+                          width: 10,
                         ),
+                        Text('visit to read more',
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 94, 79, 58))),
                       ],
                     ),
-
-                    // Input Email
-                    Container(
-                      padding: const EdgeInsets.symmetric(vertical: 10),
-                      child: const TextField(
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Email',
-                            contentPadding: EdgeInsets.symmetric(
-                                vertical: 21, horizontal: 12)),
-                      ),
-                    ),
-
-                    // Input Password
-                    Container(
-                      padding: const EdgeInsets.symmetric(vertical: 10),
-                      child: const TextField(
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Password',
-                            contentPadding: EdgeInsets.symmetric(
-                                vertical: 21, horizontal: 12)),
-                      ),
-                    ),
-
-                    // Forgot password text button
-                    forgotButton(),
-                    // Login button
-                    loginButton(),
-                    Container(
-                        margin: EdgeInsets.all(20),
-                        child: Text('New User? Create Account'))
-                  ],
-                ),
-              );
-            },
+                  ))
+            ],
           ),
-        ),
+        )),
       ),
     );
   }
-}
-
-Widget forgotButton() {
-  return Container(
-    padding: const EdgeInsets.only(bottom: 10),
-    child: TextButton(
-        style: ButtonStyle(
-            foregroundColor: WidgetStateProperty.all<Color>(Colors.blue)),
-        onPressed: () {},
-        child: const Text('Forgot Password')),
-  );
-}
-
-Widget loginButton() {
-  return SizedBox(
-    height: 45,
-    width: 240,
-    child: ElevatedButton(
-      style: const ButtonStyle(
-          backgroundColor: WidgetStatePropertyAll(Colors.blue),
-          foregroundColor: WidgetStatePropertyAll(Colors.white)),
-      onPressed: () {
-        print('Clicked!');
-      },
-      child: const Text(
-        'Login',
-        style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
-      ),
-    ),
-  );
 }
 
 class MyHomePage extends StatefulWidget {
